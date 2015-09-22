1ec28b812131bec8945a56793b5340230c14323b
Olu Oyedipe <oo185005@teradata.com>
2015-09-22 14:22:44 -0700

    adding changes to Greeting

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -1,19 +1,17 @@
 package hello;
 
-public class Greeting {
-	private final long id;
-	private final String content;
+public abstract class Greeting {
+	protected final long id;
+
 	
-	public Greeting(long id, String content) {
+	public Greeting(long id) {
 		this.id = id;
-		this.content = content;
+
 	}
 	
 	public long getId(){
 		return id;
 	}
 	
-	public String getContent(){
-		return content;
-	}
+
 }
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,12 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-	private static final String template = "Hello, %s!";
+	private static final String GreetingTemplate = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
 	@RequestMapping("/greeting")
 	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name){
-		return new Greeting(counter.incrementAndGet(), 
-			String.format(template,name));
+		return new GreetingInstance(counter.incrementAndGet(),
+			String.format(GreetingTemplate,name));
 	}
+
+    @RequestMapping("/databases")
+    public Databases getAllDatabases(){
+        //TODO should return list of all databases
+        return new Databases(counter.incrementAndGet(),Arrays.asList("prod", "dev")) ;
+    }
 }
